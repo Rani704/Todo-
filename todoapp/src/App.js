a0e9081f8ae34ce5cb773d14c2716617a5e29783
Rani <rani7042003@gmail.com>
2024-03-18 15:47:55 +0530

    first commit

---
+++ todoapp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import Resturant from './components/Basics/Resturant';
+//  import UseState from "./components/Hooks/useState";
+ import UseEffect from './components/Hooks/useEffect';
+import Todo from './components/todoreact/todo';
+// import UseReducer from './components/Hooks/useReducer';
+// import Todo from './components/todoreact/todo';
+// import Temp from './components/weather/temp.js'
+const App = () => {
+  return (
+    <>
+    <Todo/>
+    </>
+  );
+};
+
+
+export default App;
